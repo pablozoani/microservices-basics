63daa9eabbcd218c2ef473e1c973ec99d73a2aa6
Pablo Zoani <pablozoani@hotmail.com>
2021-01-07 08:06:59 -0300

    bootstrap user-service

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 
+### department-service
 ROOT_DIR=$(pwd)
 GROUP_ID=com.pzoani
-
-
-### department-service
 PROJECT_NAME=department-service
 spring init \
     --boot-version=2.4.1 \
@@ -19,7 +17,44 @@ spring init \
     --dependencies=web,data-jpa,h2,lombok,actuator,devtools \
     $PROJECT_NAME
 cd $PROJECT_NAME
-echo "FROM openjdk:11">> Dockerfile
+echo "FROM openjdk:11" >> Dockerfile
+echo src >> .dockerignore
+mkdir scripts
+cd scripts
+mkdir database
+mkdir rest-calls
+cd ..
+cd src/main/resources
+echo spring.application.name=$PROJECT_NAME >> application.properties
+echo >> application.yml
+cd ..
+cd java/*/*/*
+mkdir config
+mkdir controller
+mkdir model
+mkdir repository
+mkdir service
+cd "$ROOT_DIR"
+
+
+### user-service
+ROOT_DIR=$(pwd)
+GROUP_ID=com.pzoani
+PROJECT_NAME=user-service
+spring init \
+    --boot-version=2.4.1 \
+    --language=java \
+    --java-version=11 \
+    --build=maven \
+    --packaging=jar \
+    --name $PROJECT_NAME \
+    --groupId $GROUP_ID \
+    --artifactId $PROJECT_NAME \
+    --dependencies=web,data-jpa,h2,lombok,actuator,devtools \
+    $PROJECT_NAME
+
+cd $PROJECT_NAME
+echo "FROM openjdk:11" >> Dockerfile
 echo src >> .dockerignore
 mkdir scripts
 cd scripts
